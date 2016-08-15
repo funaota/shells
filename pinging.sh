7629642d56b90ab97019568966cbb7b5d03d1e3d
takujifunao <takuji.funao@gmail.com>
2016-08-15 12:43:24 +0900

    add pinging.sh

---
+++ pinging.sh
@@ -0,0 +1,9 @@
+echo "input your ip address only 9 digits of the front"
+read ROOT_IP
+ip=0
+timeOut=0.01
+while [ $ip -lt 255 ]
+do
+  ping -s 1 -W $timeOut -c 1 $ROOT_IP.$ip
+  ip=`expr $ip + 1`
+done
