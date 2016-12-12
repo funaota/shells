2c1bff1e792facc5b1fcea1b3fa75a9b1bc73217
funaota <takuji.funao@gmail.com>
2016-12-13 01:26:13 +0900

    fixed rails

--- rails_deploy_production.sh
+++ rails_deploy_production.sh
@@ -1,8 +1,8 @@
 bundle exec rake assets:precompile RAILS_ENV=production
 bundle exec rake db:create RAILS_ENV=production
-bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake db:migrate:reset RAILS_ENV=production
 
 DOT_FILE=".env"
 
 echo "RAILS_SERVE_STATIC_FILES=true" >> ${DOT_FILE}
-bundle exec rake secret >> ${DOT_FILE}
+echo "SECRET_KEY_BASE=${bundle exec rake secret}" >> ${DOT_FILE}
