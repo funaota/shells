ba4d37f1cfafb199d792af3123876d89fefb524e
funaota <takuji.funao@gmail.com>
2016-12-13 01:17:56 +0900

    update

--- rails_deploy_production.sh
+++ rails_deploy_production.sh
@@ -4,9 +4,5 @@ bundle exec rake db:migrate RAILS_ENV=production
 
 DOT_FILE=".env"
 
-if [ -f ${DOT_FILE} ]; then
-  echo "RAILS_SERVE_STATIC_FILES=true" >> ${DOT_FILE}
-  bundle exec rake secret >> ${DOT_FILE}
-else
-  echo "ファイルが存在しません"
-fi
+echo "RAILS_SERVE_STATIC_FILES=true" >> ${DOT_FILE}
+bundle exec rake secret >> ${DOT_FILE}
