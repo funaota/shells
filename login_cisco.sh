0c2d97723861198b047f05c666854eead88b3000
funaota <takuji.funao@gmail.com>
2017-01-25 17:52:42 +0900

    Update login_cisco.sh

--- login_cisco.sh
+++ login_cisco.sh
@@ -1,6 +1,9 @@
+READ a
+READ b
+
 curl https://1.1.1.1/login.html --insecure -X POST \
 -d "err_flag=0" \
 -d "info_flag=0" \
 -d "redirect_url=http://captive.apple.com/hotspot-detect.html" \
--d "username=takujifunao" \
--d "password=#7V7Mpez'"
+-d "username=$a" \
+-d "password=$b'"
